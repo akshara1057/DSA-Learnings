b1fdb81e2d0c5394aae67b306941ff1025c56a40
Akshara1057 <aksharagoyal18@gmail.com>
2025-12-15 15:20:47 +0530

    Fix Reverse Array: in-place reverse and correct print

--- Arrays/Reversearray.cpp
+++ Arrays/Reversearray.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 using namespace std;
 void reverse(int arr[],int n){
-    cout<<"Reverse Array is :"<<endl;
-    for(int i=n-1;i>=0;i--){
+  int start=0;
+  int end=n-1;
+  while(start<=end){
+    swap(arr[start],arr[end]);
+    start++;
+    end--;
+  }
+}
+void print(int arr[],int n){
+      cout<<"Reverse Array is :"<<endl;
+    for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
-    }
+    } 
 }
 int main(){
     int n,i;
@@ -16,4 +25,5 @@ int main(){
         cin>>arr[i];
     }
     reverse(arr,n);
+    print(arr,n);
 }
